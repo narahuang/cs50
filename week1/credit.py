9fe6d45fffa7d26b597e814ca72904968bcecb9a
Nara Huang <nara.huang@canonical.com>
2016-11-27 19:06:44 +0800

    credit.py: Complete checksum

--- week1/credit.py
+++ week1/credit.py
@@ -12,5 +12,28 @@ def getinput():
             
 if __name__ == "__main__":
     num = getinput()
-    print(type(num))
-    print(num)
+    digit = 0
+    first1 = 0
+    first2 = 0
+    summ = 0
+    icard = int(num)
+    while True:
+        digit += 1
+        now = icard % 10
+        if (digit % 2) == 1:
+            summ += now
+        else:
+            if (now * 2) < 10:
+                summ += now * 2
+            else:
+                summ += (now * 2) % 10
+                summ += ( (now*2) - ( (now*2)%10 ) ) / 10
+        first2 = first1
+        first1 = now
+        icard -= now
+        print(icard)
+        print("digit = {0}, first1 = {1}, first2 = {2}, now = {3}, summ = {4}".format(digit, first1, first2, now, summ))
+        if icard == 0:
+            break
+        icard /= 10
+    print(summ)
