986afabd83a913c2cc9fe79f291d9bf77e129099
Nara Huang <nara.huang@canonical.com>
2016-11-30 21:37:06 +0800

    W2:crack: Add crypt.c for ref

---
+++ week2/crypt.c
@@ -0,0 +1,16 @@
+#define _XOPEN_SOURCE
+#include <cs50.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <crypt.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    string password;
+    string pass = "rofl";
+    string salt = "50";
+    password = crypt(pass,salt);
+    printf("The encrypted password is: %s\n", password);
+    return 0;
+}
